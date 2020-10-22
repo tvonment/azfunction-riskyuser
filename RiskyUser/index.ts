28d260872b15fbfa0673f567ca91d8f4dc2806d1
Thomas <65050898+tvonment@users.noreply.github.com>
2020-10-22 14:04:29 +0000

    create funcitons / get UserProfiles

--- RiskyUser/index.ts
+++ RiskyUser/index.ts
@@ -2,16 +2,56 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 import axios, { AxiosRequestConfig } from 'axios';
 import qs = require('qs');
 
+
+const SUPPORT_EMAIL = "thomas@balderdash.ch"
+
+const APP_ID = "";
+const APP_SECERET = "";
+const TENANT_ID = "";
+const TOKEN_ENDPOINT = 'https://login.microsoftonline.com/' + TENANT_ID + '/oauth2/v2.0/token';
+const MS_GRAPH_SCOPE = 'https://graph.microsoft.com/.default';
+const MS_GRAPH_ENDPOINT = 'https://graph.microsoft.com/v1.0/';
+const MS_GRAPH_ENDPOINT_BETA = 'https://graph.microsoft.com/beta/';
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
 
-    const APP_ID = "";
-    const APP_SECERET = "";
-    const TENANT_ID = "";
-    const TOKEN_ENDPOINT = 'https://login.microsoftonline.com/' + TENANT_ID + '/oauth2/v2.0/token';
-    const MS_GRAPH_SCOPE = 'https://graph.microsoft.com/.default';
-    const MS_GRAPH_ENDPOINT = 'https://graph.microsoft.com/v1.0/';
+    axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
+    let token = await getToken();    
+    let riskyusers = await getRiskyUsers(token);
+    let userinfos = await getRiskyUserProfiles(token, riskyusers);
+    
+    
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: userinfos
+    };
+
+};
+
+export default httpTrigger;
+
+class RiskyUser {
+    id: string;
+    isDeleted: boolean;
+    isProcessing: false;
+    riskLevel: string;
+    riskState: string;
+    riskDetail: string;
+    riskLastUpdatedDateTime: string;
+    userDisplayName: string;
+    userPrincipalName: string;
+}
+
+class RiskyUserProfile {
+    displayName: string;
+    userPrincipalName: string;
+    mobilePhone: string;
+    otherMails: string[];
+}
+
+async function getToken(): Promise<string> {
     const postData = {
         client_id: APP_ID,
         scope: MS_GRAPH_SCOPE,
@@ -19,9 +59,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         grant_type: 'client_credentials'
     };
 
-    axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
-    let token = await axios
+    return await axios
         .post(TOKEN_ENDPOINT, qs.stringify(postData))
         .then(response => {
             console.log(response.data);
@@ -30,7 +68,9 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         .catch(error => {
             console.log(error);
         });
-    
+}
+
+async function getRiskyUsers(token:string): Promise<RiskyUser[]> {
     let config: AxiosRequestConfig = {
         method: 'get',
         url: MS_GRAPH_ENDPOINT + 'identityProtection/riskyUsers',
@@ -39,7 +79,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         }
     }
     
-    let riskyusers = await axios(config)
+    return await axios(config)
         .then(response => {
             console.log(response.data);
             return response.data.value;
@@ -47,12 +87,34 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         .catch(error => {
             console.log(error);
         });
+}
+
+async function getRiskyUserProfiles(token: string, users:RiskyUser[]): Promise<RiskyUserProfile[]> {
+    let userinfos:RiskyUserProfile[] = [];
+    for (let user of users) {
+        let config: AxiosRequestConfig = {
+            method: 'get',
+            url: MS_GRAPH_ENDPOINT_BETA + 'users/' + user.userPrincipalName + '?$select=displayName,userPrincipalName,mobilePhone,otherMails',
+            headers: {
+              'Authorization': 'Bearer ' + token //the token is a variable which holds the token
+            }
+        }
+        userinfos.push(await axios(config)
+        .then(response => {
+            console.log(response.data);
+            return response.data;
+        })
+        .catch(error => {
+            console.log(error);
+        }));
+    }
+    return userinfos;
+}
+
+async function sendMail(from:string, to:string) {
     
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: riskyusers
-    };
+}
 
-};
-
-export default httpTrigger;
+async function sendSMS() {
+    
+}
